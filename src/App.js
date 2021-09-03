ec6df17a64a30d4676d4530774c08f35a849dced
정우재 <80890472+woojae05@users.noreply.github.com>
2021-09-03 22:18:01 +0900

    create loginMainPage
    로그인 페이지 바탕 페이지 구현

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import loginMain from './components/loginMain';
+// import { Diff } from 'jest/node_modules/jest-diff/build/cleanupSemantic';
 
 function App() {
   return (
+    <Router>
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <Route exact path="/loginMain" component={loginMain}></Route>
+        </div>     
+       </header>
     </div>
-  );
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/components/loginMain.js
@@ -0,0 +1,21 @@
+import React from "react";
+import "../styles/loginMain.css";
+
+function loginMain() {
+    return (
+        <body>
+            <div className="container">
+                <div className="transparent-box"></div>
+                <div className="img"></div>
+                <div className="mainColor"></div>
+                <div className="title">
+                    <p>대소고 전용 급식 웹</p>
+                    <h1>급식의 민족</h1>
+                </div>
+            </div>
+        </body>
+
+    )
+}
+
+export default loginMain;

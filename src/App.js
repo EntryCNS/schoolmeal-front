7db39c4d3f46d5a86b3689b2b5775fcdea12836d
정우재 <80890472+woojae05@users.noreply.github.com>
2021-09-04 22:13:07 +0900

    feat: implement signin page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
-import loginMain from './components/loginMain';
+import loginMain from './components/Login/loginMain';
+import loginForm from './components/Login/loginForm';
+import { hashString } from './utils/security'
+// import LoginPage from './page/Login/LoginPage';
 // import { Diff } from 'jest/node_modules/jest-diff/build/cleanupSemantic';
 
 function App() {
+  console.log(hashString("Hello"))
+
   return (
     <Router>
     <div className="App">
       <header className="App-header">
         <div>
           <Route exact path="/loginMain" component={loginMain}></Route>
+          <Route exact path="/loginForm" component={loginForm}></Route>
         </div>     
        </header>
     </div>
---
+++ src/config/apiConfig.js
@@ -0,0 +1,6 @@
+const apiConfig = {
+    'API_ENDPOINT': 'http://10.80.163.128:8080'
+
+}
+
+export default apiConfig

b289242167c0726d6421dac5239a1cd2247727df
강성훈 <yolly050101@naver.com>
2021-09-10 17:10:34 +0900

    Meal

--- src/components/mainPage/MainPage.js
+++ src/components/mainPage/MainPage.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Nav from "./Nav";
+import SMeal from "./SMeal";
 
 const MainPage = () => {
   return (
     <div>
       <Nav />
+      <SMeal />
     </div>
   );
 };

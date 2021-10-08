f0df0ad953ab4b7db30c8b34a3bb06c49cb13d42
정우재 <80890472+woojae05@users.noreply.github.com>
2021-10-08 23:57:41 +0900

    create: menu

--- src/components/mainPage/SMeal.js
+++ src/components/mainPage/SMeal.js
@@ -1,12 +1,40 @@
 import React from "react";
 import "../../styles/mainSMeal.css";
 
+
 const SMeal = () => {
+//   const School = require('shcool-kr');
+
+//  const shcool = new School();
+
+//  const example = async function(){
+
+//   const result = await school.search(School.Region.DEAGU, '대구소프트웨어마이스터고');
+//   school.init(School.Type.HIGH, School.Region.DEAGU, result[0].schoolCode);
+  
+//   const meal = await school.getMeal();
+//   console.log(meal);
+  
+//  }
+// example();
   return (
- 
-      <div className="Meal">
-        
+
+    <div className="Meal">
+      <div className="menu">
+        <h1>오늘의 급식</h1>
+        <p>빠르게 급식을 확인하세요</p>
+        <div className="mor">
+
+        </div>
+
+        <div className="aft">
+        </div>
+
+        <div className="eve">
+        </div>
+
       </div>
+    </div>
 
   );
 };

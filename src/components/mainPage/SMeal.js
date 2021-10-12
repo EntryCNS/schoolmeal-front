b9cea860baff016ec5be0346e7036f061f59da11
정우재 <80890472+woojae05@users.noreply.github.com>
2021-10-12 23:08:47 +0900

    create: Smeal

--- src/components/mainPage/SMeal.js
+++ src/components/mainPage/SMeal.js
@@ -1,36 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../../styles/mainSMeal.css";
-
+import axios from "axios";
+import apiConfig from "../../config/apiConfig"
 
 const SMeal = () => {
-  const School = require('node-school-kr') 
-  const school = new School()
-   
-  school.init(School.Type.HIGH, School.Region.DEAGU, 'D100000282')
+  const [MonthData, SetMonthData] = useState([]);
+  const [TodayData, SetTodayData] = useState(null) 
 
-const example = async function () {
-  const meal = await school.getMeal()
-  console.log(meal);
-  console.log(meal.today)
-  
-}
-example();
+  useEffect(() => {
+    axios.get(`${apiConfig.API_ENDPOINT}/openapi/schoolmenu?month=${10}`)
+  .then((result) => {
+    const menuList = result.data.body; 
+    const todayMenu = menuList[new Date().getDate() - 1] // 배열이므로 -1
+    console.log(menuList);
+    SetTodayData(todayMenu);
+    SetMonthData(result.data.body)
+  })
+  }, [])
+
+
   return (
 
     <div className="Meal">
+
       <div className="menu">
         <h1>오늘의 급식</h1>
         <p>빠르게 급식을 확인하세요</p>
-        <div className="menuCard">
-          
-        </div>
+        {
+          TodayData==null ? <p>로딩 중..</p> :
+           (
+            <div>
+              <h1>조식</h1>
+              <div className="menuCard">
+                <p>{TodayData.breakfast.join(", ")}</p>
+              </div>
 
-        <div className="menuCard">
-        </div>
+              <h1>중식</h1>
+              <div className="menuCard">
+              <p>{TodayData.lunch.join(", ")}</p>
+              </div>
 
-        <div className="menuCard">
-        </div>
+              <h1>석식</h1>
+              <div className="menuCard">
+              <p>{TodayData.dinner.join(", ")}</p>
+              </div>
 
+            </div>
+
+          )
+        }
       </div>
     </div>
 

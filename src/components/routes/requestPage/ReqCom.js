929b2c1c22087f772c324760dbf320e955ac30c7
정우재 <80890472+woojae05@users.noreply.github.com>
2021-10-31 23:57:52 +0900

    add: writtenAt
    작성일 추가

--- src/components/routes/requestPage/ReqCom.js
+++ src/components/routes/requestPage/ReqCom.js
@@ -7,26 +7,38 @@ import axios from "axios";
 import { connect } from "react-redux";
 import { LIKE, UNLIKE } from "../../../reducer/MealReducer";
 import apiConfig from "../../../config/apiConfig";
-
+import swal from "sweetalert";
 
 const ReqCom = memo(({ index, LIKE, UNLIKE }) => {
+  let writtenDate = new Date(index.writtenAt)
+  console.log(index.writtenAt)
+  console.log(new Date(1634722554864))
   const [Vote, SetVote] = useState(0);
-  const [Admin, setAdmin] = useState(false)
+  const [Admin, setAdmin] = useState(true)
   const voteClick = () => {
     SetVote(Vote + 1);
-    axios.post(`${apiConfig.API_ENDPOINT}/api/menus/${index.id}/like`, {}, {
+    axios.post(`${apiConfig.API_ENDPOINT}/api/menus/${index.id}/like`,  {
       headers: { 'x-access-token': `Bearer ${localStorage.getItem("jwtAccessToken")}` }
     }).then(() => {
-      window.location.reload();
     }).catch(() => {
-      alert("이미 투표하셨습니다")
+      swal("이미 투표하셨습니다")
     })
   }
 
   const acceptClick =()=>{
+    swal("수락하셨습니다")
   }
   const removeClck =()=>{
-    
+    axios.delete(`${apiConfig.API_ENDPOINT}/api/menus/${index.id}`,{
+      headers:{'x-access-token': `Bearer ${localStorage.getItem("jwtAccessToken")}`}
+    }).then(()=>{
+      console.log("성공")
+    }
+    )
+    .catch(e=>{
+      console.log(e)
+      console.log(index.id)
+    })
   }
 
   return (
@@ -49,15 +61,16 @@ const ReqCom = memo(({ index, LIKE, UNLIKE }) => {
         <div className="btnContainer" style={{ marginLeft: "150px", cursor: "pointer" }}>
           <img style={{ width: "50px", marginRight: "10px" }}
             src={accept}
+            onClick={acceptClick}
           />
           <img style={{ width: "50px", cursor: "pointer" }}
             src={remove}
+            onClick={removeClck}
           />
         </div>
         ):(null)}
-
       </p>
-
+        <p className="reqDate" style={{color:"gray"}}>{writtenDate.getMonth()}월 {writtenDate.getDate()}일에 작성</p>
     </div>
   );
 });
